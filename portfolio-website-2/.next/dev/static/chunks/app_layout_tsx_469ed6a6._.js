e3e62bab67fe5346fdd890a96fca3fbbdd5613ec
YahyaAlaaMassoud <yahyaalaamassoud@gmail.com>
2025-12-03 00:22:53 -0500

    add git ignore

--- portfolio-website-2/.next/dev/static/chunks/app_layout_tsx_469ed6a6._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_6874ad75._.js",
-  "static/chunks/[root-of-the-server]__54a9bff1._.css"
-],
-    source: "dynamic"
-});
--- portfolio-website-2/.next/dev/static/chunks/app_page_tsx_b3e40d15._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_e7e9d9af._.js",
-  "static/chunks/node_modules_6d8bfdcc._.js"
-],
-    source: "dynamic"
-});
